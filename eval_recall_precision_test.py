cac40fbce60d8f0c33a5af0687c0dfdc61456516
Jinseiken AI <jinseikenai@gmail.com>
2018-06-15 14:12:05 +0900

    ndpi file free 1'st edition

--- eval_recall_precision_test.py
+++ eval_recall_precision_test.py
@@ -1,5 +1,5 @@
 # Copyright 2018 The University of Tokyo Hospital. All Rights Reserved.
-# This software includes the work that is distributed in the Apache Licence 2.0.
+# <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />This program is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
 r"""
 This program is unit of compiling the result of Faster R-CNN-Based Glomerular Detector.
 This unit detect glomeruli from multistained Whole Slide Images(WSIs) of human renal tissue sections.
@@ -193,7 +193,8 @@ class EvalRecallPrecision(AnnotationHandler):
             file_key = ''
             reader = csv.reader(list_file)
             for row in reader:
-                body, ext = os.path.splitext(row[2].replace(' ', ''))
+                # body, ext = os.path.splitext(row[2].replace(' ', ''))
+                body = row[1].replace(' ', '')
                 if file_body != body:
                     file_body = body
                     # file_key = body
--- glomus_handler.py
+++ glomus_handler.py
@@ -1,5 +1,5 @@
 # Copyright 2018 The University of Tokyo Hospital. All Rights Reserved.
-# This software includes the work that is distributed in the Apache Licence 2.0.
+# <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />This program is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
 
 r"""
 A super class of handling virtual slide files.
